81285a7518d6d70fe029b88f303a247c56203d9a
LuckyCode7 <nowak9.private@gmail.com>
2018-09-21 12:03:15 +0200

    new methods added

--- inc/list.hpp
+++ inc/list.hpp
@@ -7,10 +7,12 @@ class List
 {
 public:
     List();
+    List(const List& list);
+    List(List&& source);
     void addLast(std::shared_ptr<Node<T> > node);                                   // dodaje element na koniec listy
     void addFirst(std::shared_ptr<Node<T>> node);                                   // dodaje element na początek listy
-    std::shared_ptr<Node<T>> get(const T& value) const;                             // zwraca element o wskazanej wartości szukając od pierwszego elementu
-    std::shared_ptr<Node<T>> getBackWard(const T& value) const;                     // zwraca element o wskazanej wartości szukając od ostatniego elementu
+    std::shared_ptr<Node<T>> get(const T& value);                                   // zwraca element o wskazanej wartości szukając od pierwszego elementu
+    std::shared_ptr<Node<T>> getBackWard(const T& value);                           // zwraca element o wskazanej wartości szukając od ostatniego elementu
     std::shared_ptr<Node<T>> getHead() const;                                       // zwraca pierwszy element
     std::shared_ptr<Node<T>> getTail() const;                                       // zwraca ostatni element
     std::shared_ptr<Node<T>> getPrevious(std::shared_ptr<Node<T>> node) const;      // zwraca poprzedni element
@@ -19,11 +21,16 @@ public:
     void removeLast();                                                              // usuwa ostatni element
     int count() const;                                                              // zwraca liczbę wszystkich elementów listy
     int count(const T& value) const;                                                // zwraca liczbę konkretnych elementów listy
-    void show() const;                                                              // pokazuje elementy listy
+    void replace(const T& target, const T& value);                                  // zamienia wartość węzła
+    void show();                                                                    // pokazuje elementy listy
+    void clear();                                                                   // czyści listę
     ~List() {}
 private:
     std::shared_ptr<Node<T>> first;
     std::shared_ptr<Node<T>> last;
+    bool checkNextAndPreviousValues(std::shared_ptr<Node<T>> node);
+    bool checkIfNodeIsNotFirst(std::shared_ptr<Node<T>> node);
+    bool checkIfListIsEmpty();
 };
 
 template<class T>
@@ -33,10 +40,36 @@ List<T>::List() :
 {}
 
 template<class T>
+List<T>::List(const List& list) :
+    first(list.getHead()),
+    last (list.getTail())
+{}
+
+template<class T>
+List<T>::List(List&& source) :
+    first(source.getHead()),
+    last (source.getTail())
+{
+    source.clear();
+}
+
+template<class T>
 void List<T>::removeFirst()
 {
     first = first->next;
     first->prev.lock() = nullptr;
+}
+
+template<class T>
+void List<T>::clear()
+{
+    std::shared_ptr<Node<T>> current = last;
+    while(current != nullptr)
+    {
+        current->next.reset();
+        current = current->prev.lock();
+    }
+    first.reset();
 }
 
 template<class T>
@@ -47,17 +80,33 @@ void List<T>::removeLast()
 }
 
 template<class T>
-void List<T>::show() const
+void List<T>::show()
 {
-    std::cout<<"nullptr<->";
     std::shared_ptr<Node<T>> current = first;
-    do
+    if(!checkIfListIsEmpty())
     {
-        std::cout<<current->value<<"<->";
+        std::cout<<"nullptr<->";
+        do
+        {
+            std::cout<<current->value<<"<->";
+            current = current->next;
+        }
+        while(current != nullptr);
+        std::cout<<"nullptr"<<std::endl;
+    }
+    else throw EmptyListError();
+}
+
+template<class T>
+void List<T>::replace(const T& target, const T& value)
+{
+    std::shared_ptr<Node<T>> current = first;
+    while(current != nullptr)
+    {
+        if(current->value == target)
+            current->value = value;
         current = current->next;
     }
-    while(current != nullptr);
-    std::cout<<"nullptr"<<std::endl;
 }
 
 template<class T>
@@ -81,50 +130,61 @@ int List<T>::count(const T& value) const
     while(current != nullptr)
     {
         if(current->value == value)
-        ++howMany;
+            ++howMany;
         current = current->next;
     }
     return howMany;
 }
 
 template<class T>
-void List<T>::addLast(std::shared_ptr<Node<T>> node)
+bool List<T>::checkNextAndPreviousValues(std::shared_ptr<Node<T>> node)
 {
     if(node->next != nullptr || node->prev.lock() != nullptr)
     {
         throw InvalidNextOrPrevValue();
     }
-    if(first == nullptr) //pierwszy węzeł
+    return true;
+}
+
+template<class T>
+bool List<T>::checkIfNodeIsNotFirst(std::shared_ptr<Node<T>> node)
+{
+    if(first == nullptr)
     {
         first = node;
         last = node;
+        return true;
     }
-    else
+    return false;
+}
+
+template<class T>
+bool List<T>::checkIfListIsEmpty()
+{
+    if(first == nullptr)
     {
-        std::shared_ptr<Node<T>> current = first;
+        throw EmptyListError();
+    }
+    return false;
+}
+
+template<class T>
+void List<T>::addLast(std::shared_ptr<Node<T>> node)
+{
+    checkNextAndPreviousValues(node);
+    if(!checkIfNodeIsNotFirst(node))
+    {
+        last->next = node;
         node->prev = last;
         last = node;
-        while(current->next != nullptr)
-        {
-            current = current->next;
-        }
-        current->next = node;
     }
 }
 
 template<class T>
 void List<T>::addFirst(std::shared_ptr<Node<T>> node)
 {
-    if(node->next != nullptr)
-    {
-        throw InvalidNextOrPrevValue();
-    }
-    if(first == nullptr)
-    {
-        first = node;
-        last = node;
-    }
-    else
+    checkNextAndPreviousValues(node);
+    if(!checkIfNodeIsNotFirst(node))
     {
         first->prev = node;
         node->next = first;
@@ -133,13 +193,9 @@ void List<T>::addFirst(std::shared_ptr<Node<T>> node)
 }
 
 template<class T>
-std::shared_ptr<Node<T>> List<T>::get(const T& value) const
+std::shared_ptr<Node<T>> List<T>::get(const T& value)
 {
-    if(first == nullptr)
-    {
-        throw EmptyListError();
-    }
-    else
+    if(!checkIfListIsEmpty())
     {
         std::shared_ptr<Node<T>> current = first;
         do
@@ -160,13 +216,9 @@ std::shared_ptr<Node<T>> List<T>::get(const T& value) const
 }
 
 template<class T>
-std::shared_ptr<Node<T>> List<T>::getBackWard(const T& value) const
+std::shared_ptr<Node<T>> List<T>::getBackWard(const T& value)
 {
-    if(first == nullptr)
-    {
-        throw EmptyListError();
-    }
-    else
+    if(!checkIfListIsEmpty())
     {
         std::shared_ptr<Node<T>> current = last;
         do
